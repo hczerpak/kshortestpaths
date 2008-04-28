861ba3dbb31105296b5d7db6a508e69752ee0909
Hubert.Czerpak <Hubert.Czerpak@db32cc0e-cf40-0410-a46c-bd64b149a481>
2008-04-28 19:35:38 +0000

    Wersja która już wypluwa xml'a z wynikami

---
+++ trunk/KShortestPaths/test/pl/czerpak/system/LuncherTestCase.java
@@ -0,0 +1,24 @@
+package pl.czerpak.system;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LuncherTestCase {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testMain() {
+		Luncher.main(new String [] {  });
+	}
+
+}
